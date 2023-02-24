6135401566a23ba366c4e2501417039d3da2de2d
saajanrana <123057148+saajanrana@users.noreply.github.com>
2023-02-25 02:58:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,21 @@
-var btn=document.getElementById("btn");
-var numPtag=document.getElementById("num");
-var res=document.getElementById("respond");
-var inputValue=document.getElementById("guess").value;
-function randomNumGenerator(){
-randomNum=Math.floor(Math.random()*41)-20;
-	document.getElementById("num").innerText=randomNum;
-	guessNum();
-	print();
-	
-}
-function guessNum(){
-	input=Number(document.getElementById("guess").value);
-}
-function print(){
-	if(Math.abs(randomNum)-input)<=5){
-		document.getElementById("respond").innerText="Hot";
-	}
-	else{
-		document.getElementById("respond").innerHTML="Cold";
-	}
-}
-btn.addEventListener("click",randomNumGenerator);
 
+var randomNum;
+var input;
+
+function randomNumGenerator() {
+  randomNum = Math.floor(Math.random() * 41) - 20;
+  document.getElementById("num").innerText = randomNum;
+  guessNum();
+  print();
+}
+
+function guessNum() {
+  input = Number(document.getElementById("guess").value);
+}
+function print() {
+  if (Math.abs(randomNum - input) <= 5) {
+    document.getElementById("respond").innerText = "Hot";
+  } else {
+    document.getElementById("respond").innerHTML = "Cold";
+  }
+}
