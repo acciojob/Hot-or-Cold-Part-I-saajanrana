a8f357e163a89bd9b4ad828674c804f3a85522cd
saajanrana <123057148+saajanrana@users.noreply.github.com>
2023-04-24 21:21:38 -0400

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,26 @@
 
-var randomNum;
-var input;
+let secretNumber = Math.floor(Math.random() * 100) + 1;
+let previousGuess = null;
 
-
-
-function randomNumGenerator() {
-  randomNum = Math.floor(Math.random() * 41) - 20;
-  document.getElementById("num").innerText = randomNum;
-  guessNum();
-  print();
-}
-
-function guessNum() {
-  input = Number(document.getElementById("guess").value);
-}
-function print() {
-  if (Math.abs(randomNum - input) <= 5) {
-    document.getElementById("respond").innerText = "Hot";
+function guessNumber() {
+  const guess = parseInt(document.getElementById("guess").value);
+  let message = "";
+  if (guess === secretNumber) {
+    message = "Congratulations! You guessed the number!";
   } else {
-    document.getElementById("respond").innerHTML = "Cold";
+    const difference = Math.abs(guess - secretNumber);
+    const previousDifference = previousGuess ? Math.abs(previousGuess - secretNumber) : difference + 1;
+    if (difference < previousDifference) {
+      message = "Getting hotter!";
+    } else {
+      message = "Getting colder!";
+    }
+    if (guess < secretNumber) {
+      message += " Guess higher!";
+    } else {
+      message += " Guess lower!";
+    }
+    previousGuess = guess;
   }
+  document.getElementById("response").textContent = message;
 }
-randomNumGenerator(input);
