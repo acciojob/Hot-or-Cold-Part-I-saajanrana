9aca6570103e3b482fb5b226396e46292f873826
Saajan Singh Rana <123057148+saajanrana@users.noreply.github.com>
2023-06-27 05:20:16 -0400

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,19 @@
 var randomNum;
 var input;
-function randomNumGenerator()
-{
-    randomNum = Math.floor(Math.random()*41) - 20;
-    document.getElementById("num").innerHTML = randomNum;
-    guessName();
-    print();
+function randomNumGenerator(){
+	randomNum = Math.floor(Math.random()*41) - 20;
+	document.getElementById("num").innerHTML = randomNum;
+	guessName();
+	print();
 }
-
-function guessName()
-{
-    input = parseInt(document.getElementById("guess").value);
+function guessName() {
+	input = parseInt(document.getElementById("guess").value);	
 }
-
-function print()
-{
-    if(Math.abs(randomNum - input) <= 5)
-    {
-        document.getElementById("respond").innerHTML = "Hot";
-    }
-
-    else 
-    {
-        document.getElementById("respond").innerHTML = "Cold";
-    }
+function print(){
+	if(Math.abs(randomNum - input) <= 5){
+		document.getElementById("respond").innerHTML = "Hot";
+	}
+	else{
+		document.getElementById("respond").innerHTML = "Cold";
+	}
 }
